9953d0aacb9fe3ed9f0ac24f4b7755e330783fcf
Adam Johnston <ajohnston1219@gmail.com>
2019-07-17 11:32:30 -0500

    diffChangeLog script added

--- liquibase-diffChangeLog.bat
+++ liquibase-diffChangeLog.bat
@@ -1,4 +1,6 @@
-liquibase diff ^
+liquibase ^
+--changeLogFile=%1 ^
+diffChangeLog ^
 --referenceUrl=jdbc:postgresql://reference-db.c15thsg7cjkl.us-east-1.rds.amazonaws.com/reference_db ^
 --referenceUsername=postgres ^
 --referencePassword=secretPassword
